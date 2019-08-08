63f169bb9f0416662facd6a3d529006c0c9df7e1
mcueto <cueto@live.cl>
2019-08-08 01:56:07 -0400

    Create UnderConstruction component

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>
+      {{ title }}
+    </h1>
+
+    <p>
+      {{ description }}
+    </p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UnderConstruction',
+  props: {
+    title: String,
+    description: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
