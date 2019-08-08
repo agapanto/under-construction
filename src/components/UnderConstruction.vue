4ad4c061ff5b9f598a22b6b194b931ebe3ddb40f
mcueto <cueto@live.cl>
2019-08-08 02:36:35 -0400

    Add social icons to UnderConstruction component
    - Register fontawesome icons on main.js
    - Render social media icons on UnderConstruction component

--- src/components/UnderConstruction.vue
+++ src/components/UnderConstruction.vue
@@ -8,6 +8,28 @@
       {{ description }}
     </p>
 
+    <div class="row">
+      <div class="col-md-12">
+
+        <div class="social-icons text-center my-4">
+          <a class="px-4" href="#">
+            <font-awesome-icon :icon="['fab', 'facebook-f']" />
+          </a>
+          <a class="px-4" href="#">
+            <font-awesome-icon :icon="['fab', 'instagram']" />
+          </a>
+          <a class="px-4" href="#">
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </a>
+        </div>
+
+      </div>
+    </div>
+
+    <div>
+
+    </div>
+
   </div>
 </template>
 
@@ -23,4 +45,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.social-icons {
+  font-size: 3rem;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faFacebookF)
+library.add(faInstagram)
+library.add(faGithub)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
