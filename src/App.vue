3b154d77c59dc7a7a2c8457b60f6dc44cde611a8
mcueto <cueto@live.cl>
2019-08-08 01:59:06 -0400

    CSS changes
    Now it's beauty

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,41 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Bungee&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Biryani&display=swap');
+
+body {
+  background-color: #4d0059;
+  background-image: url("https://www.transparenttextures.com/patterns/shattered.png");
+  /* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Biryani', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
 }
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Bungee', cursive;
+  color: white;
+}
+
+footer {
+  margin-top: 8rem;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a:hover {
+  color: white;
+  text-decoration: underline;
+}
+
 </style>
