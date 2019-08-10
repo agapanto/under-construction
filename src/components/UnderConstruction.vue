fcf2790f3094e8f58673849d6aafad1140487bf4
mcueto <cueto@live.cl>
2019-08-10 17:02:07 -0400

    Render social network icons based on it's env vars

--- src/components/UnderConstruction.vue
+++ src/components/UnderConstruction.vue
@@ -12,15 +12,11 @@
       <div class="col-md-12">
 
         <div class="social-icons text-center my-4">
-          <a class="px-4" href="#">
-            <font-awesome-icon :icon="['fab', 'facebook-f']" />
+
+          <a class="px-4" :href="social_network.url" v-bind:key="social_network.id" v-for="social_network in social_networks">
+            <font-awesome-icon :icon="['fab', social_network.icon ]" />
           </a>
-          <a class="px-4" href="#">
-            <font-awesome-icon :icon="['fab', 'instagram']" />
-          </a>
-          <a class="px-4" href="#">
-            <font-awesome-icon :icon="['fab', 'github']" />
-          </a>
+
         </div>
 
       </div>
@@ -39,6 +35,64 @@ export default {
   props: {
     title: String,
     description: String
+  },
+  mounted: () => {
+  },
+  computed: {
+    social_networks: () => {
+      let enabled_social_networks = []
+
+      if (process.env.VUE_APP_FACEBOOK_URL) {
+        enabled_social_networks.push(
+          {
+            "id": "facebook",
+            "name": "facebook",
+            "icon": "facebook-f",
+            "url": process.env.VUE_APP_FACEBOOK_URL
+          }
+        )
+      }
+
+      if (process.env.VUE_APP_INSTAGRAM_URL) {
+        enabled_social_networks.push(
+          {
+            "id": "instagram",
+            "name": "instagram",
+            "icon": "instagram",
+            "url": process.env.VUE_APP_INSTAGRAM_URL
+          }
+        )
+      }
+
+      if (process.env.VUE_APP_TWITTER_URL) {
+        enabled_social_networks.push(
+          {
+            "id": "twitter",
+            "name": "twitter",
+            "icon": "twitter",
+            "url": process.env.VUE_APP_TWITTER_URL
+          }
+        )
+      }
+
+      if (process.env.VUE_APP_GITHUB_URL) {
+        enabled_social_networks.push(
+          {
+            "id": "github",
+            "name": "github",
+            "icon": "github",
+            "url": process.env.VUE_APP_GITHUB_URL
+          }
+        )
+      }
+
+      return enabled_social_networks
+    }
+  },
+  data: () => {
+    return {
+    }
+
   }
 }
 </script>
