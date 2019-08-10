118023bd793f7ebdb3b0bc0fa8f1096600addab3
mcueto <cueto@live.cl>
2019-08-10 17:01:09 -0400

    Add twitter icon import to fontawesome library

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faFacebookF)
 library.add(faInstagram)
+library.add(faTwitter)
 library.add(faGithub)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
