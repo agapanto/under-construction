c3e9f3430ccb0092f71061c858004f01ed0a02fa
Marcelo Cueto <cueto@live.cl>
2020-01-02 21:56:43 -0300

    Add envelope icon to icon library

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faFacebookF)
 library.add(faInstagram)
 library.add(faTwitter)
 library.add(faGithub)
+library.add(faEnvelope)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
