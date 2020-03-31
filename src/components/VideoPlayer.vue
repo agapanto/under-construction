3d7536dad9ee33e0a778d5d5c252727135aa41b1
Marcelo Cueto <cueto@live.cl>
2020-03-31 10:55:16 -0300

    Create VideoPlayer vue component

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="video_player">
+
+    <div class="row">
+      <div class="col-md-12">
+
+        <div class="embed-responsive embed-responsive-16by9 my-4">
+          <iframe class="embed-responsive-item" v-bind:src="video_url" frameborder="0" allow="gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'VideoPlayer',
+  props: {
+    video_url: String
+  },
+  mounted: () => {
+  },
+  computed: () => {
+  },
+  data: () => {
+    return {
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
